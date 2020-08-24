4593f622c441208d3900293a9ec16076eecc9387
Danilo Pianini <danilo.pianini@unibo.it>
2020-08-24 13:44:27 +0200

    Upgrade refreshVersions to 0.9.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,18 @@
-import de.fayard.dependencies.bootstrapRefreshVersionsAndDependencies
+import de.fayard.refreshVersions.bootstrapRefreshVersions
+import org.danilopianini.VersionAliases.justAdditionalAliases
 
 buildscript {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
     repositories { gradlePluginPortal() }
-    dependencies.classpath("de.fayard:dependencies:+")
+    dependencies {
+        classpath("de.fayard.refreshVersions:refreshVersions:0.9.5")
+        classpath("org.danilopianini:refreshversions-aliases:+")
+    }
 }
 
-bootstrapRefreshVersionsAndDependencies()
+bootstrapRefreshVersions(justAdditionalAliases)
 
 rootProject.name = "listset"
